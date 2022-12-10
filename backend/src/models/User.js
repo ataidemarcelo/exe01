6f4fd1d221840b1764bb73367e4108b7dfbd75fa
Marcelo Ataíde <msa.trybe@gmail.com>
2022-12-10 10:48:03 -0300

    feat(back): create user model

---
+++ backend/src/models/User.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    passwordHash: DataTypes.STRING,
+    image: DataTypes.STRING
+  }, {
+    underscored: true,
+    timestamps: true,
+    tableName: 'users'
+  });
+
+  return User;
+};
