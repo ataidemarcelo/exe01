67a014f5ab9571bd18a847cd46dd2519968d9e0a
Marcelo Ataíde <msa.trybe@gmail.com>
2022-12-15 11:36:43 -0300

    feat(front) - persists user theme preference in LocalStorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,7 @@
-import { useState, createContext } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { ThemeProvider } from './context/theme.context';
 import MainHeader from './components/MainHeader';
-
 import HomePage from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
@@ -10,25 +9,17 @@ import SignUp from './pages/SignUp';
 
 import './global.css';
 
-export const ThemeContext = createContext(null);
-
 function App() {
-  const [theme, setTheme] = useState('light');
-
-  const toggleTheme = () => setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
-
   return (
-    <ThemeContext.Provider value={ { theme, toggleTheme } }>
-      <div className="wrapper" id={theme}>
-        <MainHeader />
-        <Switch>
-          <Route path="/" exact component={ HomePage } />
-          <Route path="/dashboard" component={ Dashboard } />
-          <Route path="/signin" component={ SignIn } />
-          <Route path="/signup" component={ SignUp } />
-        </Switch>
-      </div>
-    </ThemeContext.Provider>
+    <ThemeProvider>
+      <MainHeader />
+      <Switch>
+        <Route path="/" exact component={ HomePage } />
+        <Route path="/dashboard" component={ Dashboard } />
+        <Route path="/signin" component={ SignIn } />
+        <Route path="/signup" component={ SignUp } />
+      </Switch>
+    </ThemeProvider>
   );
 }
 
---
+++ frontend/src/context/theme.context.js
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext(null);
+
+const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    const theme = localStorage.getItem('@BlogAPI:theme:');
+
+    if (theme) {
+      return theme;
+    }
+
+    return 'light';
+  });
+
+  const toggleTheme = () => {
+    setTheme((curr) => curr === 'light' ? 'dark' : 'light');
+
+    // Ver como usar de forma assincrona o setTheme 
+    const newTheme = theme === 'light' ? 'dark' : 'light';
+    localStorage.setItem('@BlogAPI:theme:', newTheme)
+
+    console.log(theme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="wrapper" id={theme}>
+        {children}
+      </div>
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => {
+  const context = useContext(ThemeContext);
+
+  if (!context) {
+    throw new Error('useTheme must be used within a ThemeProvider');
+  }
+
+  return context;
+};
+
+export { ThemeProvider, useTheme };
