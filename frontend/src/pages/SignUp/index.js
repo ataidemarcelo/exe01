664a46898d5a74ba805e7859d27b1499903b6bfb
Marcelo Ataíde <msa.trybe@gmail.com>
2022-12-17 07:57:34 -0300

    feat(front) - validate form data

--- frontend/src/pages/SignUp/index.js
+++ frontend/src/pages/SignUp/index.js
@@ -1,35 +1,62 @@
 import React from 'react';
 import { useState } from 'react';
+import * as Yup from 'yup';
+
 import styles from  './signup.module.css';
 
 function SignUp() {
   const [dataForm, setDataForm] = useState({ name: '', email: '', password: '', passwordConfirmation: '' });
+  const [error, setError] = useState('');
 
-  const handleChange = (event) => {
+  const handleChange = async (event) => {
     const { name, value } = event.target;
     const copyFormData = { ...dataForm };
     
     copyFormData[name] = value;
+
     setDataForm(copyFormData);
   }
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(dataForm);
-  };
 
+    try {
+      const schema = Yup.object().shape({
+        passwordConfirmation: Yup.string().oneOf(
+          [Yup.ref('password'), undefined],
+          'Senha e confirmação de senha precisam ser iguais',
+        ),
+        password: Yup.string()
+          .min(6, 'A senha precisa ter no mínimo 6 caracters')
+          .required('O campo password é obrigatório.'),
+        email: Yup.string()
+          .email('Digite um email válido')
+          .required('O campo email é obrigatório.'),
+        name: Yup.string()
+          .required('O campo nome é obrigatório.'),
+      });
+
+      await schema.validate(dataForm);
+      setError('');
+    } catch (err) {
+      const { errors } = err;
+      setError(errors[0]);
+    }
+    
+  };
 
   return (
     <main className={styles.main}>
+      <h1>Cadastro</h1>
       <form onSubmit={ handleSubmit } className={styles.form}>
         <label>
-          Name:
+          Nome:
           <input 
             type="text" 
             name="name"
             value={ dataForm.name }
             placeholder="Nome"
-            onChange={handleChange }
+            onChange={ handleChange }
           />
         </label>
 
@@ -40,7 +67,7 @@ function SignUp() {
             name="email"
             value={ dataForm.email }
             placeholder="Email"
-            onChange={handleChange }
+            onChange={ handleChange }
           />
         </label>
 
@@ -68,6 +95,9 @@ function SignUp() {
 
         <button type="submit" >Cadastrar</button>
       </form>
+      <span className={styles.errors}>
+        {error && error}
+      </span>
     </main>
   );
 };
