70281f8d9a9cb574eef1252d99acbc5f4fbc66ea
Marcelo Ataíde <msa.trybe@gmail.com>
2022-12-17 09:31:43 -0300

    feat(front) - add cors and route NOT_FOUND

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 require('express-async-errors');
 require('dotenv').config();
+const cors = require('cors');
 
 const routers = require('./routes/routers');
 const { errorMiddleware } = require('./middlewares');
@@ -8,9 +9,14 @@ const { errorMiddleware } = require('./middlewares');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routers);
 
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'Pong!' }));
+
+app.use('/*', (req, res) => {
+  res.status(404).json({ message: `Not found: ${req.baseUrl}` });
+});
 
 app.use(errorMiddleware);
 
