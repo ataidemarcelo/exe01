493c5d52c11a4fadab7ca602d17378c0fddcc68d
Marcelo Ataíde <msa.trybe@gmail.com>
2022-12-27 10:12:40 -0300

    feat(front) - create posts page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import HomePage from './pages/Home';
 import Dashboard from './pages/Dashboard';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Posts from './pages/Posts';
 
 import './global.css';
 
@@ -18,6 +19,8 @@ function App() {
         <Route path="/dashboard" component={ Dashboard } />
         <Route path="/signin" component={ SignIn } />
         <Route path="/signup" component={ SignUp } />
+
+        <Route path="/posts" component={ Posts } />
       </Switch>
     </ThemeProvider>
   );
