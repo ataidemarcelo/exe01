92f62639516d5dd06e7b2bc951c94dc44b7f6ede
Marcelo Ataíde <msa.trybe@gmail.com>
2023-12-26 11:30:27 -0300

    feat(back) add route /status

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,6 +14,15 @@ app.use(routers);
 
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'Pong!' }));
 
+app.get('/status', (_req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+
+  res.status(200).json({ message: '[Healthy] - API Up!' })
+  next();
+});
+
 app.use('/*', (req, res) => {
   res.status(404).json({ message: `Not found: ${req.baseUrl}` });
 });
